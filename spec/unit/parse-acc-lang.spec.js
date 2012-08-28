536d3e28318b7b1163c48484317c425bba018c65
Waldemar Schwan <waldemar.schwan@adcloud.com>
2012-08-28 10:27:50 +0200

    changed naming to follow a typical nodejs style more closly

--- spec/unit/parse-acc-lang.spec.js
+++ spec/unit/parse-acc-lang.spec.js
@@ -21,61 +21,60 @@
 // SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 "use strict";
 
-var extract_first_accept_language = require("lib/parse-acc-lang").extract_first_accept_language;
-var extract_all_accept_languages = require("lib/parse-acc-lang").extract_all_accept_languages;
+var accLangParser = require("lib/parse-acc-lang");
 
 describe("accept-language http header parser", function () {
 	describe("in case of valid headers", function () {
 
-		describe("when only one language is of interest extract_first_accept_language", function () {
+		describe("when only one language is of interest accLangParser.extractFirstLang", function () {
 			it("should extract the highest ranked language", function () {
 				var acc_lang_header_content = "de";
-				var result = extract_first_accept_language(acc_lang_header_content);
+				var result = accLangParser.extractFirstLang(acc_lang_header_content);
 				expect(result.language).toEqual("de");
 			});
 
 			it("should not return any locale if not present", function () {
 				var acc_lang_header_content = "de";
-				var result = extract_first_accept_language(acc_lang_header_content);
+				var result = accLangParser.extractFirstLang(acc_lang_header_content);
 				expect(result.locale).toBeUndefined();
 			});
 
 			it("should extract the highest ranked language and locale", function () {
 				var acc_lang_header_content = "de-DE";
-				var result = extract_first_accept_language(acc_lang_header_content);
+				var result = accLangParser.extractFirstLang(acc_lang_header_content);
 				expect(result.language).toEqual("de");
 				expect(result.locale).toEqual("DE");
 			});
 
 			it("should ensure that the language is in lower case", function () {
 				var acc_lang_header_content = "DE-DE";
-				var result = extract_first_accept_language(acc_lang_header_content);
+				var result = accLangParser.extractFirstLang(acc_lang_header_content);
 				expect(result.language).toEqual("de");
 			});
 
 			it("should ensure that the locale is in upper case", function () {
 				var acc_lang_header_content = "de-de";
-				var result = extract_first_accept_language(acc_lang_header_content);
+				var result = accLangParser.extractFirstLang(acc_lang_header_content);
 				expect(result.locale).toEqual("DE");
 			});
 
 			it("should handle the special any char (*) correctly", function () {
 				var acc_lang_header_content = "*";
-				var result = extract_first_accept_language(acc_lang_header_content);
+				var result = accLangParser.extractFirstLang(acc_lang_header_content);
 				expect(result.language).toEqual("*");
 			});
 
 			it("should not have a locale in case of *", function () {
 				var acc_lang_header_content = "*";
-				var result = extract_first_accept_language(acc_lang_header_content);
+				var result = accLangParser.extractFirstLang(acc_lang_header_content);
 				expect(result.locale).toBeUndefined();
 			});
 		});
 
-		describe("when multiple languages are of interest extract_all_accept_languages", function () {
+		describe("when multiple languages are of interest accLangParser.extractAllLangs", function () {
 			it("should parse multiple languages like: de, en", function () {
 				var acc_lang_header_content = "de, en";
-				var result = extract_all_accept_languages(acc_lang_header_content);
+				var result = accLangParser.extractAllLangs(acc_lang_header_content);
 
 				expect(result.length).toEqual(2);
 
@@ -85,7 +84,7 @@ describe("accept-language http header parser", function () {
 			
 			it("should parse multiple languages with locale", function () {
 				var acc_lang_header_content = "de-DE, en-GB";
-				var result = extract_all_accept_languages(acc_lang_header_content);
+				var result = accLangParser.extractAllLangs(acc_lang_header_content);
 
 				expect(result.length).toEqual(2);
 
@@ -96,9 +95,9 @@ describe("accept-language http header parser", function () {
 				expect(result[1].locale).toEqual("GB");
 			});
 			
-			it("should parse multiple languages with locale and priority extract_all_accept_languages", function () {
+			it("should parse multiple languages with locale and priority accLangParser.extractAllLangs", function () {
 				var acc_lang_header_content = "de-DE,de;q=0.8,en-US;q=0.6,en;q=0.4";
-				var result = extract_all_accept_languages(acc_lang_header_content);
+				var result = accLangParser.extractAllLangs(acc_lang_header_content);
 
 				expect(result.length).toEqual(4);
 
@@ -117,54 +116,54 @@ describe("accept-language http header parser", function () {
 		});
 	});
 
-	describe("in case of missing headers extract_all_accept_languages", function () {
+	describe("in case of missing headers accLangParser.extractAllLangs", function () {
 		it("should be resilient to undefined input", function () {
 			var acc_lang_header_content;
-			var result = extract_all_accept_languages(acc_lang_header_content);
+			var result = accLangParser.extractAllLangs(acc_lang_header_content);
 
 			expect(result).toEqual([]);
 		});
 		
 		it("should be resilient to empty headers", function () {
 			var acc_lang_header_content = "";
-			var result = extract_all_accept_languages(acc_lang_header_content);
+			var result = accLangParser.extractAllLangs(acc_lang_header_content);
 
 			expect(result).toEqual([]);
 		});
 	});
 
-	describe("in case of invalid headers extract_all_accept_languages", function () {
+	describe("in case of invalid headers accLangParser.extractAllLangs", function () {
 		it("should be resilient to chines jibberisch: 8痂", function () {
 			var acc_lang_header_content = "8痂";
-			var result = extract_all_accept_languages(acc_lang_header_content);
+			var result = accLangParser.extractAllLangs(acc_lang_header_content);
 
 			expect(result).toEqual([]);
 		});
 
 		it("should be resilient to none 2-ALPHA languages like: ded", function () {
 			var acc_lang_header_content = "ded";
-			var result = extract_all_accept_languages(acc_lang_header_content);
+			var result = accLangParser.extractAllLangs(acc_lang_header_content);
 
 			expect(result).toEqual([]);
 		});
 
 		it("should be resilient to jibberisch", function () {
 			var acc_lang_header_content = "§$$&de";
-			var result = extract_all_accept_languages(acc_lang_header_content);
+			var result = accLangParser.extractAllLangs(acc_lang_header_content);
 
 			expect(result).toEqual([]);
 		});
 
 		it("should be resilient to -", function () {
 			var acc_lang_header_content = "-";
-			var result = extract_all_accept_languages(acc_lang_header_content);
+			var result = accLangParser.extractAllLangs(acc_lang_header_content);
 
 			expect(result).toEqual([]);
 		});
 		
 		it("should be resilient to numbers", function () {
 			var acc_lang_header_content = "12364";
-			var result = extract_all_accept_languages(acc_lang_header_content);
+			var result = accLangParser.extractAllLangs(acc_lang_header_content);
 
 			expect(result).toEqual([]);
 		});
